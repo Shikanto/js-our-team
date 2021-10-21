afb2cc0fc25284625bac71e570cb5381c48e35df
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-21 18:22:49 +0200

    ex. done

--- script.js
+++ script.js
@@ -10,30 +10,68 @@ const membersTeam = [
 
 const teamContainer = document.querySelector(".team-container");
 
+function addCardMember(memberName, memberRole, memberImage) {
+    teamContainer.innerHTML += `<div class="team-card">
+                                        <div class="card-image">
+                                            <img
+                                                src="img/${memberImage}"
+                                                alt="${memberName}"
+                                            />
+                                        </div>
+                                        <div class="card-text">
+                                            <h3>${memberName}</h3>
+                                            <p>${memberRole}</p>
+                                        </div>
+                                    </div>`
+}
+
+function addNewMember() {
+    
+    const newName = document.getElementById("name").value;
+    const newRole = document.getElementById("role").value;
+    const newImage = document.getElementById("image").value;  
+    const infoNewMember = {
+
+    } ;
+
+    infoNewMember.name = newName;
+    infoNewMember.role = newRole;
+    infoNewMember.image = newImage;
+    console.log(infoNewMember);
+    
+    membersTeam.push(infoNewMember);
+    
+    addCardMember(newName, newRole, newImage);
+    
+}
+
+
+
+
 for (let i= 0; i < membersTeam.length; i++){
 
     const singleMember = membersTeam[i];
-    console.log(singleMember)
+    /* console.log(singleMember) */
 
     let memberName = singleMember.name;
-    console.log(memberName);
+    /* console.log(memberName); */
 
     let memberRole = singleMember.role;
-    console.log(memberRole);
+    /* console.log(memberRole); */
 
     let memberImage = singleMember.image;
-    console.log(memberImage);
+    /* console.log(memberImage); */
 
-    teamContainer.innerHTML += `<div class="team-card">
-                                    <div class="card-image">
-                                        <img
-                                            src="img/${memberImage}"
-                                            alt="${memberName}"
-                                        />
-                                    </div>
-                                    <div class="card-text">
-                                        <h3>${memberName}</h3>
-                                        <p>${memberRole}</p>
-                                    </div>
-                                </div>`
+    addCardMember(memberName, memberRole, memberImage);
+
 }
+
+const btnAdd =  document.getElementById("addMemberButton");
+
+btnAdd.addEventListener("click", addNewMember);
+
+
+
+
+/* cardNewMember = addCardMember(); */
+
